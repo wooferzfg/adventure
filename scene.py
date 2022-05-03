f13f5bbe076229675d6f392159a6865c2f61a3b8
wooferzfg <spapushin@gmail.com>
2022-05-03 05:06:59 -0400

    Add Intro scene

--- scene.py
+++ scene.py
@@ -1,16 +1,38 @@
 from manim import *
 from keyboard import draw_keyboard_create, draw_qwerty_keyboard, draw_dungeon_keyboard
+from adventure import AdventureGame
 
 
-class CreateQwertyKeyboard(Scene):
+MAIN_FONT = 'Century Gothic'
+TERMINAL_FONT = 'Consolas'
+
+
+class AdventureScene(Scene):
     def construct(self):
         self.camera.background_color = WHITE
+        self.draw_scene()
+
+
+class Intro(AdventureScene):
+    def draw_scene(self):
+        text = AdventureGame().get_current_output()
+        text_element = Paragraph(text, color=BLACK, width=12, font_size=96, line_spacing=3, font=TERMINAL_FONT)
+        self.play(Write(text_element, run_time=6))
+        self.pause(2)
+        title_element = Text("Adventure", color=BLACK, font_size=160, font=MAIN_FONT).move_to(UP * 0.8)
+        self.play(FadeOut(text_element, run_time=3), GrowFromCenter(title_element, run_time=3, lag_ratio=0.03))
+        subtitle_element = Text("Galactic Puzzle Hunt 2018", color=BLACK, font_size=60, font=MAIN_FONT).move_to(DOWN * 1.2)
+        self.play(FadeIn(subtitle_element, run_time=2))
+        self.pause(5)
+
+
+class CreateQwertyKeyboard(AdventureScene):
+    def draw_scene(self):
         outlines, texts = draw_qwerty_keyboard()
         draw_keyboard_create(self, outlines, texts)
 
 
-class CreateDungeonKeyboard(Scene):
-    def construct(self):
-        self.camera.background_color = WHITE
+class CreateDungeonKeyboard(AdventureScene):
+    def draw_scene(self):
         outlines, texts = draw_dungeon_keyboard()
         draw_keyboard_create(self, outlines, texts)
